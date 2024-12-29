c8d3f371d0daa20d0550acd4f00699aa21a4a318
ArkcV <andrielmarques@hotmail.com>
2024-12-28 22:15:39 -0300

    feat add tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,32 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        white: {
+          cf_white: "#FFFFFF",
+        },
+        black: {
+          cf: "#2B2B2B",
+        },
+        yellow: {
+          cf_yellow: "#DEBA3B",
+        },
+        blue: {
+          cf_blue: "#1A1363",
+        },
+        purple: {
+          cf_purple1: "#77749B",
+          cf_purple2: "#332F64",
+        },
+        gray: {
+          cf_gray1: "#ECE9E9",
+          cf_gray2: "#969292",
+          cf_gray3: "#4B4B4B",
+        },
       },
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
     },
   },
   plugins: [],
