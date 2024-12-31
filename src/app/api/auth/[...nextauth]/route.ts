3ec4aaa9ea7a7a90c1ca68fb16c56aec1a03746c
ArkcV <andrielmarques@hotmail.com>
2024-12-31 15:39:30 -0300

    feat: add security in autentification Api

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,20 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
   pages: {
-    signIn: "/"
+    signIn: "/",
   },
   providers: [
     CredentialsProvider({
       name: "Credentials",
       credentials: {
         email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" }
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
         if (!credentials) {
-          return null
+          return null;
         }
 
         if (credentials.email === "User@gmail.com" && credentials.password === "123456Am") {
@@ -22,12 +22,25 @@ const handler = NextAuth({
             id: "1",
             name: "User",
             email: "User@gmail.com",
-          }
+          };
         }
         return null;
-      }
-    })
-  ]
-})
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`, 
+      options: {
+        httpOnly: true, 
+        sameSite: "strict", 
+        path: "/",
+      },
+    },
+  },
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
