6b95dff4b7e1f484636396e30dd965d9afdcd521
ArkcV <andrielmarques@hotmail.com>
2025-01-04 15:43:38 -0300

    feat : add initial style Sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -9,6 +9,7 @@ import { MdOutlineInventory2 } from "react-icons/md";
 import { CgGym } from "react-icons/cg";
 import LogoutButton from "./LogoutButton";
 import { Session } from "next-auth";
+import Image from "next/image";
 
 
 interface SideBarProps {
@@ -32,36 +33,55 @@ const menuItems = [
 ];
 
 
-export default async function Sidebar({session}: SideBarProps) {
-
+export default async function Sidebar({ session }: SideBarProps) {
   return (
-    <>
+    <div className="w-16 md:w-56 fixed left-0 top-0 z-10 h-screen border pt-8 px-4 bg-blue-cf_blue">
       <nav>
-      <div>
-      {session ? (
-        <div>{session.user?.name}
-          <div>{session.user?.email}</div>
+        <div  className="flex flex-col items-center">
+
+        <div className="mb-2">
+          <Image
+            src="/person-circle-side.png"
+            alt="crossflex-logo"
+            width={50}
+            height={100}
+            priority
+          />
         </div>
-      ) : (
-        <div>Please log in</div>
-      )}
-    </div>
-      <ul>
-        {menuItems.map((cat, index) => (
-          <li key={index}>
-            <ul>
-              {cat.list.map((item) => (
-                <li key={item.path}>
-                  {item.icon}
-                  <a href={item.path}>{item.title}</a>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
-      <LogoutButton />
+      
+        <div className="text-center mb-4 ">
+          {session ? (
+            <div>
+              <div >{session.user?.name}</div>
+              <div>{session.user?.email}</div>
+            </div>
+          ) : (
+            <div className="text-sm">Please log in</div>
+          )}
+        </div>
+      
+
+        </div>
+        <ul>
+          {menuItems.map((cat, index) => (
+            <li key={index}>
+              <ul>
+                {cat.list.map((item) => (
+                  <li key={item.path}>
+                    {item.icon}
+                    <a href={item.path}>
+                      {item.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <LogoutButton />
+        </div>
       </nav>
-    </>
+    </div>
   );
 }
